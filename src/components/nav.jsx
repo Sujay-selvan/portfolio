92766cb412a7dae877510794936c64a8850f7fd2
sujay-selvan <sujayu192015@gmail.com>
2025-05-17 12:22:23 +0530

    fix-navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,10 +9,10 @@ function Menu(){
         <div className="menu bg-black text-white flex justify-between item-center p-2 flex-col lg:flex-row lg:justify-between relative">
             <div className="flex justify-between">
             <p className="text-[30px]">Portfolio</p>
-            <TiThMenu className="text-[3rem] lg:hidden" onClick={()=>setHidenav(!hideNav)}/>
+            <TiThMenu className="text-[3rem] text-white lg:hidden" onClick={()=>setHidenav(!hideNav)}/>
             </div>
             
-            <div className={`flex flex-col text-xl m-auto items-center gap-8 text-[1.5rem] absolute ${hideNav?"top-[60px] w-full z-1 p-5 m-0 left-0":"top-[-400px] w-full z-1"} lg:flex-row lg:w-fit lg:relative lg:top-0 lg:m-0`}> 
+            <div className={`bg-black flex flex-col text-xl m-auto items-center gap-8 text-[1.5rem] absolute ${hideNav?"top-[60px] w-full z-1 p-5 m-0 left-0":"top-[-400px] w-full z-1"} lg:flex-row lg:w-fit lg:relative lg:top-0 lg:m-0`}> 
                 <a href="#home">Home</a>
                 <a href="#about">About</a>
                 <a href="#skill">Skill</a>
